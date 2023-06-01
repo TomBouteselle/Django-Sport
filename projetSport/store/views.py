cea1773461f3b89a7dc81bf96c719e376eaf9463
Ayakhin_ <103850784+Ayakhin@users.noreply.github.com>
2023-06-01 10:56:42 +0200

    modification du projet

---
+++ projetSport/store/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.db import models
+# Create your views here.
+"""
+Product
+
+- Nom
+- Prix  
+- Quantité en stock
+- Description
+- Image
+
+"""
+class Product(models.Model):
+    nom = models.CharField(max_length=128)
+    price = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
